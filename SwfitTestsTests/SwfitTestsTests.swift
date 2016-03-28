005f6b4b675521b98aaff303f191378a0f404df1
daniel ogorek <acosk8er@yahoo.com>
2016-03-28 17:15:26 -0400

    red: example 1 failing

--- SwfitTestsTests/SwfitTestsTests.swift
+++ SwfitTestsTests/SwfitTestsTests.swift
@@ -58,6 +58,15 @@ class SwfitTestsTests: QuickSpec {
                 }
             })
             
+            context("EX 1: when passed a marker and 5 misaligned chips", { 
+                it("should not unlock panel") {
+                    let chipsArray = ["blue,green", "blue,yellow", "red,orange", "red,green", "yellow,red", "orange,purple"]
+                    let returnedOrder = panel.unlockPanelWithChipsAndMarker(chipsArray)
+                    
+                    expect(returnedOrder).to(equal("Something"))
+                }
+            })
+            
             context("when passed a marker color and a non-matching chip", {
                 it("cannot unlock panel") {
                     let chipsArray = ["blue,green", "blue,red"]
