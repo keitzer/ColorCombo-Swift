defd3119b8caafef31bcf6cc5ddbd709deb13a8c
daniel ogorek <acosk8er@yahoo.com>
2016-03-28 16:24:47 -0400

    green: first test passing

--- SwfitTests/SecurityPanel.swift
+++ SwfitTests/SecurityPanel.swift
@@ -11,7 +11,16 @@ import Foundation
 class SecurityPanel {
     
     func unlockPanelWithChipsAndMarker(chipsAndMarkerArray: [String]) -> String {
-        return ""
+        if chipsAndMarkerArray.count < 2 {
+            return "Cannot unlock master panel"
+        }
+        
+        if chipsAndMarkerArray[0] == chipsAndMarkerArray[1] {
+            return chipsAndMarkerArray[1]
+        }
+        else {
+            return "Cannot unlock master panel"
+        }
     }
 }
 
