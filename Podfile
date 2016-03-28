4753f81ff563918a402dc73e154088385f3e6521
daniel ogorek <acosk8er@yahoo.com>
2016-03-28 16:02:32 -0400

    initial commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment this line to define a global platform for your project
+ platform :ios, '8.0'
+
+use_frameworks!
+target 'SwfitTests' do
+	pod 'Quick', '~> 0.9.0'
+	pod 'Nimble', '~> 3.2.0'
+end
+
+target 'SwfitTestsTests' do
+	pod 'Quick', '~> 0.9.0'
+	pod 'Nimble', '~> 3.2.0'
+end
+
--- SwfitTestsTests/SwfitTestsTests.swift
+++ SwfitTestsTests/SwfitTestsTests.swift
@@ -8,29 +8,18 @@
 
 import XCTest
 @testable import SwfitTests
+import Quick
+import Nimble
 
-class SwfitTestsTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+class SwfitTestsTests: QuickSpec {
+    override func spec() {
+        describe("Security Panel") {
+            context("when passed a marker and an identical chip", {
+                it("unlocks the panel") {
+                    let panel = SecurityPanel()
+                    
+                }
+            })
         }
     }
-    
 }
